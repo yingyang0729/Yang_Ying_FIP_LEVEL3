6891ffedff67d2e8cdddff7df5e1d266dfb96f7e
yingyang0729 <90883576+yingyang0729@users.noreply.github.com>
2022-12-04 05:08:20 -0500

    Update and rename main_About.js to main_Home.js

--- js/main_Home.js
+++ js/main_Home.js
@@ -7,7 +7,7 @@
             console.log('vue instance is created');
             //fetch the remote date here and pass it to the data object
 
-            fetch('./data.json')
+            fetch('./data1.json')
                 .then(res => res.json())
                 .then(data => {
                     console.log(data);
@@ -24,10 +24,10 @@
           return {
             welcome: 'My Skill, My Passion, My Strengh',
             topic: 'My name is Ying. I list all my skills as below. You can click the belwo button if you want to make an order.',
-
+            copyright: 'All prices and services on this website and in our reservation system are based on conditions at the time of publishing and are subject to change.<br> Please call to confirm services, availability and pricing.<br><br> ©2022 Ying Yang  Digital Design Inc. All Rights Reserved. Privacy Policy',
             //this is a array//
-            //profs: ['joe','john','justin','jarrod'],//
-            member: ['Graphic','Education','Video','Writing','Marketing'],
+            
+            member: ['Adobe Illustrator, Adobe Photoshop, Adobe Indesign, Adobe XD','Adobe Premiere, Adobe After Effect','Cinema 4D','CSS, Java Script, SASS, VUE JS, AJAX', 'PHP'],
             remoteData: {}
 
           }
