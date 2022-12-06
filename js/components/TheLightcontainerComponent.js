403855094c6c362f4c5e236d39c26145003d8f77
yingyang0729 <90883576+yingyang0729@users.noreply.github.com>
2022-12-06 04:06:59 -0500

    Update TheLightcontainerComponent.js

--- js/components/TheLightcontainerComponent.js
+++ js/components/TheLightcontainerComponent.js
@@ -5,50 +5,60 @@ export default {
 
     template:
     `
-    <section ref="lbWrapper" class="lightcontainer" :style="{ 'background-image': 'url(../images/'+ hero.lbBG +')'}" >
-    <!--validate date here-->
     
-        <img @click="closeLB" src="images/closeIcon.png" class="lightcontainer_close">
+    <section ref="lbWrapper" class="lightcontainer"  >
+        
+           
         <!-- hero image on the left, text on the right -->
         <!-- here image is a background image -->
-        <article>
-        <div class="package">
-            <h2 class="lb_topic">
-                <pre>{{ hero.topic }}</pre>
-            </h2>
+            
+            <div>
+                <img :src='"images/"+ hero.detail' loading="lazy" alt="" class="teamImagesD">              
+            </div>
+            <div class="lightbox-wrapper">
+                <div>
+                    <h2 class="lb_topic">
+                        <pre>{{ hero.topic }}</pre>
+                    </h2>
 
-            <h3 class="lb_heading">
-                <pre>{{ hero.Description }}</pre>
-            </h3>
-           
-            <p class="lb_text">
-               <pre>{{ hero.text }}</pre>
-            </p>
-            <h3 class="lb_heading">
-            <pre>{{ hero.timename }}</pre>
-            </h3>
-            <p class="lb_text">
-            <pre>{{ hero.time }}</pre>
-            </p>
-            <h3 class="lb_heading">
-            <pre>{{ hero.toolname }}</pre>
-            </h3>
-            <p class="lb_text">
-            <pre>{{ hero.tool }}</pre>
-            </p>
-            <h3 class="lb_heading">
-            <pre>{{ hero.type }}</pre>
-            </h3>
-            <p class="lb_text">
-            <pre>{{ hero.price }}</pre>
-            </p>
-            <p class="lb_text">
-                <pre>{{ hero.infor }}</pre>
-            </p>
-         
-        </div>
-        </article>
+                    <h3 class="lb_heading">
+                        <pre>{{ hero.Description }}</pre>
+                    </h3>
+                        
+                    <p class="lb_text">
+                        <pre>{{ hero.text }}</pre>
+                    </p>
+                    <h3 class="lb_heading">
+                        <pre>{{ hero.timename }}</pre>
+                    </h3>
+                    <p class="lb_text">
+                        <pre>{{ hero.time }}</pre>
+                    </p>
+                    <h3 class="lb_heading">
+                        <pre>{{ hero.toolname }}</pre>
+                    </h3>
+                    <p class="lb_text">
+                        <pre>{{ hero.tool }}</pre>
+                    </p>
+                    <h3 class="lb_heading">
+                        <pre>{{ hero.type }}</pre>
+                    </h3>
+                    <p class="lb_text">
+                        <pre>{{ hero.price }}</pre>
+                    </p>
+                    <p class="lb_text">
+                        <pre>{{ hero.infor }}</pre>
+                    </p>
+                </div>
+            </div>
+            
+            <div>
+                <img @click="closeLB" src="images/closeIcon.png" class="lightcontainer_close">
+            </div>    
+
+            
     </section>
+    
     `,
 
     methods: {
