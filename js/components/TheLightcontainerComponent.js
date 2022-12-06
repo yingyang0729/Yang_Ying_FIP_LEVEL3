f65d7a8258283c5b3295c2a39c8d1af4eebba72a
yingyang0729 <90883576+yingyang0729@users.noreply.github.com>
2022-12-05 23:33:51 -0500

    Update TheLightcontainerComponent.js

--- js/components/TheLightcontainerComponent.js
+++ js/components/TheLightcontainerComponent.js
@@ -10,18 +10,43 @@ export default {
     
         <img @click="closeLB" src="images/closeIcon.png" class="lightcontainer_close">
         <!-- hero image on the left, text on the right -->
-        <!-- her image is a background image -->
+        <!-- here image is a background image -->
         <article>
-            <h3 class="lb_heading">
+        <div class="package">
+            <h2 class="lb_topic">
                 <pre>{{ hero.topic }}</pre>
+            </h2>
+
+            <h3 class="lb_heading">
+                <pre>{{ hero.Description }}</pre>
             </h3>
            
             <p class="lb_text">
-               <pre>{{ hero.price }}</pre>
+               <pre>{{ hero.text }}</pre>
+            </p>
+            <h3 class="lb_heading">
+            <pre>{{ hero.timename }}</pre>
+            </h3>
+            <p class="lb_text">
+            <pre>{{ hero.time }}</pre>
+            </p>
+            <h3 class="lb_heading">
+            <pre>{{ hero.toolname }}</pre>
+            </h3>
+            <p class="lb_text">
+            <pre>{{ hero.tool }}</pre>
+            </p>
+            <h3 class="lb_heading">
+            <pre>{{ hero.type }}</pre>
+            </h3>
+            <p class="lb_text">
+            <pre>{{ hero.price }}</pre>
             </p>
             <p class="lb_text">
                 <pre>{{ hero.infor }}</pre>
             </p>
+         
+        </div>
         </article>
     </section>
     `,
